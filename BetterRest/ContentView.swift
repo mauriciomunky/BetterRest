2f82868fe4336c80d4f5784950b3f0dfd9a243ed
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-11 17:58:55 -0300

    App is working, missing UI upgrades

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,17 +5,55 @@
 //  Created by Maurício Costa on 11/01/23.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour,.minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Sua hora ideal de sono é..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
         }
-        .padding()
+        showingAlert = true
+    }
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Quando você quer acordar?").font(.headline)
+                
+                DatePicker("Por favor escolha um horário", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
+                
+                Text("Quantidade de sono desejada").font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) horas", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Quantidade diária de café").font(.headline)
+                
+                Stepper(coffeeAmount == 1 ? "1 chícara" : "\(coffeeAmount) chícaras", value: $coffeeAmount, in: 1...20)
+            }        .toolbar {
+                Button("Calcular", action: calculateBedtime)
+        }.alert(alertTitle, isPresented: $showingAlert) {
+            Button("OK") { }
+        } message: {
+            Text(alertMessage)
+        }
+        }
     }
 }
 
