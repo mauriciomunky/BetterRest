a4189def683051a369657ec1de658f28c68dc53b
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-11 16:41:58 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Maurício Costa on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
